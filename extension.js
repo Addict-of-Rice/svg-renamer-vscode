5d8a972f8c03cedf43dc80df59bec6a4ddd72df3
admond <admondtamang@gmail.com>
2023-10-24 17:32:16 +0545

    fix: add command to preview

--- extension.js
+++ extension.js
@@ -1,11 +1,12 @@
-const vscode = require("vscode");
+import * as fs from "fs";
+import * as vscode from "vscode";
 
 function activate(context) {
   console.log(
     'Congratulations, your extension "rename-svg-attributes" is now active!'
   );
 
-  let replaceSvgAttributes = vscode.commands.registerCommand(
+  const replaceSvgAttributes = vscode.commands.registerCommand(
     "rename-svg-attributes.replaceSvgAttributes",
     () => {
       const editor = vscode.window.activeTextEditor;
@@ -35,13 +36,51 @@ function activate(context) {
       vscode.window.showInformationMessage(`ohh yeah!`);
     }
   );
+
+  const svgPrevieDisposable = vscode.commands.registerCommand(
+    "extension.previewSvg",
+    () => {
+      const svgPath = vscode.window.activeTextEditor.document.uri.fsPath;
+      if (svgPath) {
+        const svgContent = fs.readFileSync(svgPath, "utf8");
+        const panel = vscode.window.createWebviewPanel(
+          "svgPreview",
+          "SVG Preview",
+          vscode.ViewColumn.One,
+          {}
+        );
+
+        panel.webview.html = getWebviewContent(svgContent);
+      }
+    }
+  );
+
+  context.subscriptions.push(svgPrevieDisposable);
   context.subscriptions.push(replaceSvgAttributes);
 }
 
+function getWebviewContent(svgContent) {
+  return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>SVG Preview</title>
+        </head>
+        <body>
+            <div>
+                ${svgContent}
+            </div>
+        </body>
+        </html>
+    `;
+}
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
   activate,
   deactivate,
+  getWebviewContent,
 };
